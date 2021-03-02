5451702db5319c122ed387407e103e2e2d9e7a4f
gandzytm <gandzy1984@gmail.com>
2021-03-02 15:24:37 +0500

    bot telegram

--- tg_bot.py
+++ tg_bot.py
@@ -5,7 +5,9 @@ bot = telebot.TeleBot('993217806:AAHsXrVb3uRKX3P7R7Au5mYhJKueqEh0UxM')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Для использования бота напишите интересующий вас вопрос.\n'
+                                      'Например:\n'
+                                      'Телефон охраны')
 
 
 @bot.message_handler(content_types=['text'])
@@ -14,6 +16,8 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет, мой создатель')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'телефон охраны':
+        bot.send_message(message.chat.id, '+79538241398')
 
 
 bot.polling()
